6c4849d1957c83237aa7dc2228c7bdceee8999f2
QinYYY <qinyang_c@outlook.com>
2024-09-14 10:08:19 +0930

    prepare for this project

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by QinY on 14/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnowSeeker/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  SnowSeeker
+//
+//  Created by QinY on 14/9/2024.
+//
+
+import Foundation
